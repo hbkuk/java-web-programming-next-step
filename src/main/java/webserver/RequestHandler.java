4ecce79ac09ee929e84f138b63a4f2589047a3fd
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-03-09 17:47:38 +0900

    [update] GET 방식으로 회원가입하기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -9,15 +9,20 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
 import java.nio.file.Files;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import model.User;
+import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
 
     private Socket connection;
-
+    
     public RequestHandler(Socket connectionSocket) {
         this.connection = connectionSocket;
     }
@@ -27,25 +32,30 @@ public class RequestHandler extends Thread {
                 connection.getPort());
 
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
-            // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
-        	
         	BufferedReader br = new BufferedReader(new InputStreamReader(in, "UTF-8"));
         	
         	String line = br.readLine();
-        	
-        	String[] tokens = line.split(" ");
-        	
-        	String url = tokens[1];
-        	
-        	while( !"".equals(line) ) {
-        		line = br.readLine();
-        		log.debug(line);
+        	if( line == null ) {
+        		return;
         	}
         	
+        	String url = HttpRequestUtils.getUrl(line);
+        	
+        	if( url.contains("/user/create")) {
+        		int index = url.indexOf("?");
+        		String queryString = url.substring((index+1));
+        		Map<String, String> params = HttpRequestUtils.parseQueryString(queryString);
+        		User user = new User( params.get("userId"), params.get("password"), params.get("name"), params.get("email") );
+        		log.debug( "User : {}", user );  
+        		url = "/index.html";
+        	}
+        	
+            	
             DataOutputStream dos = new DataOutputStream(out);
             byte[] body = Files.readAllBytes( new File( "./webapp" + url).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+        	
         } catch (IOException e) {
             log.error(e.getMessage());
         }
