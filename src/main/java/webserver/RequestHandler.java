fe61648edfd9be21139a74b17085f9fead3cfc2e
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-03-16 23:17:02 +0900

    [update] POST 방식의 요청 처리 및 302 status 응답

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -16,6 +16,7 @@ import org.slf4j.LoggerFactory;
 
 import model.User;
 import util.HttpRequestUtils;
+import util.IOUtils;
 
 public class RequestHandler extends Thread {
     private static final Logger log = LoggerFactory.getLogger(RequestHandler.class);
@@ -38,32 +39,51 @@ public class RequestHandler extends Thread {
         	
         	String[] tokens = line.split(" ");
         	
+        	int contentLength = 0;
         	while( (line = br.readLine()) != null ) {
+        		log.debug("Request Header : {}", line);
+        		
+        		if( line.contains("Content-Length")) {
+        			contentLength = Integer.parseInt( line.split(": ")[1] );
+        		}
+        		
         		if( line.equals("")) {
         			break;
         		}
         	}
-        	String[] uri = tokens[1].split("\\?");
-        	log.debug("requestURL : {} ",line);
-        	String requestPath = uri[0];
-        	String parameters = uri[1];
+        	String requestURI = tokens[1]; 
         	
-        	Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(parameters);
+        	if( requestURI.contains("/user/create")) {
         	
-        	User user = new User( 
-        			parameterMap.get("userId"), 
-        			parameterMap.get("password"), 
-        			parameterMap.get("name"), 
-        			parameterMap.get("email") );
+        		String requestBody = IOUtils.readData(br, contentLength);
+        		
+        		Map<String, String> parameterMap = HttpRequestUtils.parseQueryString(requestBody);
+        		
+        		User user = new User(
+        				parameterMap.get("userId"),
+        				parameterMap.get("password"),
+        				parameterMap.get("name"),
+        				parameterMap.get("email") );
+        		
+        		log.debug("User Model : {}", user.toString());
+        		
+        		requestURI = "/index.html";
+        		
+                DataOutputStream dos = new DataOutputStream(out);
+                
+                byte[] body = Files.readAllBytes(Paths.get("./webapp" + requestURI));
+                
+                response302Header(dos, requestURI);
+                responseBody(dos, body);
+        	} else {
+                DataOutputStream dos = new DataOutputStream(out);
+                
+                byte[] body = Files.readAllBytes(Paths.get("./webapp" + requestURI));
+                
+                response200Header(dos, body.length);
+                responseBody(dos, body);
+        	}
         	
-        	log.debug("User Model : {}", user.toString());
-        	
-            DataOutputStream dos = new DataOutputStream(out);
-            
-            byte[] body = Files.readAllBytes(Paths.get("./webapp" + requestPath));
-            
-            response200Header(dos, body.length);
-            responseBody(dos, body);
         } catch (IOException e) {
             log.error(e.getMessage());
         }
@@ -74,6 +94,16 @@ public class RequestHandler extends Thread {
             dos.writeBytes("HTTP/1.1 200 OK \r\n");
             dos.writeBytes("Content-Type: text/html;charset=utf-8\r\n");
             dos.writeBytes("Content-Length: " + lengthOfBodyContent + "\r\n");
+            dos.writeBytes("\r\n");
+        } catch (IOException e) {
+            log.error(e.getMessage());
+        }
+    }
+    
+    private void response302Header(DataOutputStream dos, String requestURI) {
+        try {
+            dos.writeBytes("HTTP/1.1 302 Found \r\n");
+            dos.writeBytes("Location: " + requestURI );
             dos.writeBytes("\r\n");
         } catch (IOException e) {
             log.error(e.getMessage());
