4d26830c4d05d540337c54b68d517391719ea53b
qudrnr1025@gmail.com <109803585+hbkuk@users.noreply.github.com>
2023-04-03 13:29:14 +0900

    리플렉션 실습

--- src/test/java/core/ref/Junit4TestRunner.java
+++ src/test/java/core/ref/Junit4TestRunner.java
@@ -1,11 +1,19 @@
 package core.ref;
 
+import java.lang.reflect.Method;
+
 import org.junit.Test;
 
 public class Junit4TestRunner {
     @Test
     public void run() throws Exception {
         Class<Junit4Test> clazz = Junit4Test.class;
+        Method[] method = clazz.getDeclaredMethods();
+        for (int i = 0; i < method.length; i++) {
+			if(method[i].isAnnotationPresent(MyTest.class)) {
+				method[i].invoke(clazz.newInstance());
+			}
+		}
 
     }
 }
